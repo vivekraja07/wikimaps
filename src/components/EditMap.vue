b01e2848f0d18c706c9c6d8b99f55bda82ee63dd
vivekraja07 <vivekraja07@gmail.com>
2019-10-26 17:09:57 -0400

    new changes

---
+++ src/components/EditMap.vue
@@ -0,0 +1,121 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader';
+import { mapActions } from 'vuex'
+
+
+export default {
+  name: 'edit-map',
+  
+  methods: {
+    ...mapActions([
+      'linkSet'
+    ]),
+  },
+  mounted() {
+    // lazy load the required ArcGIS API for JavaScript modules and CSS
+    loadModules([
+      'esri/Map',
+      'esri/views/MapView',
+      "esri/layers/FeatureLayer",
+      "esri/widgets/Editor",
+      "esri/widgets/TimeSlider"
+       ], { css: true })
+    .then(([ArcGISMap, MapView, FeatureLayer, Editor, TimeSlider]) => {    
+      const map = new ArcGISMap({
+        basemap: 'topo-vector',
+      });
+
+       var featureLayer = new FeatureLayer({
+         url: "https://services5.arcgis.com/rQJwj1ctcaOp5BYz/arcgis/rest/services/wikimaps/FeatureServer/0",
+        //  popupEnabled: false,
+        //  popupTemplate: {
+        //    title: "Details",
+        //    content: "This is the content of the region"
+        //  }
+        outFields: ["startYear", "endYear", "wikiLink", "EmpireName", "Information"]
+       });
+      
+      var layer = featureLayer;
+       
+     
+      map.add(featureLayer);
+      //  map.add(graphicsLayer);
+
+      this.view = new MapView({
+        container: this.$el,
+        map: map,
+        center: [23.4162, 25.6628],
+        zoom: 4
+    
+      });
+
+      var editor = new Editor({
+        view  : this.view
+      });
+
+      var timeSlider = new TimeSlider({
+        view: this.view,
+        mode: "cumulative-from-start"
+      });
+      
+      this.view.ui.add(editor, "top-right")
+      this.view.ui.add(timeSlider, "bottom-left")
+
+      let timeLayerView;
+      this.view.whenLayerView(layer).then(function(layerView) {
+      timeLayerView = layerView;
+      const fullTimeExtent = layer.timeInfo.fullTimeExtent;
+      const start = fullTimeExtent.start;
+
+      // set up time slider properties based on layer timeInfo
+      timeSlider.fullTimeExtent = fullTimeExtent;
+      timeSlider.values = [start];
+      timeSlider.stops = {
+        interval: layer.timeInfo.interval
+      };
+    });
+
+    timeSlider.watch("timeExtent", function(value){
+      // update layer view filter to reflect current timeExtent
+      timeLayerView.filter = {
+        timeExtent: value
+      };
+});
+   let that = this
+    var view = this.view
+    this.view.on("click", function(event) {
+      view.hitTest(event).then(function(response) {
+        const graphic = response.results.filter(function(result) {
+          return result.graphic.layer === featureLayer;
+        })[0].graphic;
+        const wiki = graphic.attributes.wikiLink;
+        console.log(wiki)
+        
+        that.linkSet(wiki)
+      });
+    });
+
+    });
+  },
+  beforeDestroy() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+div {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 import EditMap from '../components/EditMap'
 import ViewMap from '../components/ViewMap'
@@ -8,11 +7,6 @@ import ViewMap from '../components/ViewMap'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
   {
     path: '/view',
     name: 'view-Map',
@@ -22,14 +16,6 @@ const routes = [
     path: '/edit',
     name: 'edit-map',
     component: EditMap
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
